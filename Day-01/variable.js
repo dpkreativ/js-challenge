6414e2feeaf14f4af2409579d77ad3e0901cf837
Divine <dpkreativ@gmail.com>
2020-12-22 19:59:59 +0100

    create js file for task and completed task

---
+++ Day-01/variable.js
@@ -0,0 +1,6 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let name = "Divine"; // a string
+let awake = true; // a boolean
+let eyeColor; // undefined
+let boobs = null; // null
