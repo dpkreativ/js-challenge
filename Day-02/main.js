383f17610a78cb7a13318d26933a2da9dabcecb7
Divine Philip <dpkreativ@gmail.com>
2020-12-27 13:33:09 +0100

    exercise 1 complete

--- Day-02/main.js
+++ Day-02/main.js
@@ -80,4 +80,59 @@ Exercises: Level 3
 
 // === Exercise 1 ===
 
-/*1*/
+/*1*/ let challenge = "30 Days of JavaScript";
+
+/*2*/ console.log(challenge);
+
+/*3*/ console.log(challenge.length);
+
+/*4*/ console.log(challenge.toUpperCase());
+
+/*5*/ console.log(challenge.toLowerCase());
+
+/*6*/ console.log(challenge.substr(0, 2));
+
+/*7*/ let stringLength = challenge.length - 1;
+console.log(challenge.substr(3, stringLength));
+
+/*8*/ console.log(challenge.includes("Script"));
+
+/*9*/ console.log(challenge.split());
+
+/*10*/ console.log(challenge.split(" "));
+
+/*11*/ let techCompanies =
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(techCompanies.split(","));
+
+/*12*/ console.log(challenge.replace("JavaScript", "Python"));
+
+/*13*/ console.log(challenge.charAt(15));
+
+/*14*/ console.log(challenge.charCodeAt("8"));
+
+/*15*/ console.log(challenge.indexOf("a"));
+
+/*16*/ console.log(challenge.lastIndexOf("a"));
+
+/*17*/ let funnyQuote =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(funnyQuote.indexOf("because"));
+
+/*18*/ console.log(funnyQuote.lastIndexOf("because"));
+
+/*19*/ console.log(funnyQuote.search("because"));
+
+/*20*/ let challengeTitle = " 30 Days of JavaScript ";
+console.log(challengeTitle.trim());
+
+/*21*/ console.log(challenge.startsWith("3"));
+
+/*22*/ console.log(challenge.endsWith("t"));
+
+/*23*/ console.log(challenge.match(/a/gi));
+
+/*24*/ let phrase = "30 Days of ";
+console.log(phrase.concat("JavaScript"));
+
+/*25*/ console.log(challenge.repeat(2));
