ce375fcd5ecdc3de4d5fc3658c0904d9e451722a
Divine <dpkreativ@gmail.com>
2021-02-12 20:36:50 +0100

    ex 1 num 3

--- Day-08/main.js
+++ Day-08/main.js
@@ -10,3 +10,15 @@ Print the the dog object on the console
 
 === Solution ===
 */ console.log(dog);
+
+/*3
+Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+
+=== Solution ===
+*/ dog.legs =
+  "4";
+dog.color = "brown";
+dog.age = 4;
+dog.bark = function () {
+  return "woof woof";
+};
