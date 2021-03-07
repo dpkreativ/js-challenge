60a186b9c5b941fc41b667fb2a52c2fea6de6072
Divine <dpkreativ@gmail.com>
2021-03-07 10:29:19 +0100

    set some questions to practice on

--- Day-12/script.js
+++ Day-12/script.js
@@ -1,1 +1,36 @@
 // JavaScript Practice from Rithm School... Might replace Asabeneh's tutorials or work together with it
+
+/*1
+Create the following variables
+- name, which is a string set to your current name
+- dayOfBirth, which is a number set to the day of your birth month
+
+=== Solution ===
+*/
+const name = "Divine Philip";
+const dayOfBirth = "10";
+
+/*2
+See what happens when you have multiple variables of the same name. Which one takes precedence?
+*/
+
+/*3
+Write some JavaScript that prompt's the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+*/
+
+/*4
+Create a string that contains both single quotes and double quotes.
+*/
+
+/*5
+What is the difference between null and undefined?
+*/
+
+/*6
+What is NaN in JavaScript? What is the typeof NaN?
+*/
+
+/*7
+You can declare a variable by typing let thing;. What is the value of thing?
+
+*/
