8672b3c7140c40df6e4fc65425899f307e3ec964
Divine <dpkreativ@gmail.com>
2021-03-07 10:41:17 +0100

    finished exercises for variables

--- Day-12/script.js
+++ Day-12/script.js
@@ -1,5 +1,6 @@
 // JavaScript Practice from Rithm School... Might replace Asabeneh's tutorials or work together with it
 
+// ========= { VARIABLES AND PRIMITIVES } =========
 /*1
 Create the following variables
 - name, which is a string set to your current name
@@ -12,25 +13,45 @@ const dayOfBirth = "10";
 
 /*2
 See what happens when you have multiple variables of the same name. Which one takes precedence?
+
+=== Solution ===
+The final one
 */
 
 /*3
 Write some JavaScript that prompt's the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+
+=== Solution ===
 */
+// let favColor = prompt("enter your favorite color:");
+// console.log(favColor);
 
 /*4
 Create a string that contains both single quotes and double quotes.
+
+=== Solution ===
 */
+const randomString = "'Hello Leah', he said";
+console.log(randomString);
 
 /*5
 What is the difference between null and undefined?
+
+=== Solution ===
+null signifies the intentional absence of data while undefined is a variable  that has been declared but without value yet.
 */
 
 /*6
 What is NaN in JavaScript? What is the typeof NaN?
+
+=== Solution ===
+NaN means that a value is not a number and cannot be converted to a number... the typeof NaN is number
 */
+console.log(parseInt("bread"));
 
 /*7
 You can declare a variable by typing let thing;. What is the value of thing?
 
+=== Solution ===
+The value of thing is "undefined"
 */
