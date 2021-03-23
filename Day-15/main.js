a733206dd46665b645e0de0a0a82c1bbfe847929
Divine <dpkreativ@gmail.com>
2021-03-23 07:30:02 +0100

    ex 1 number 1 finished

---
+++ Day-15/main.js
@@ -0,0 +1,33 @@
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+
+// const person = new Person("Joseph", "Price");
+// console.log(person.firstName);
+
+// Exercise 1
+/*
+Create an Animal class. The class will have name, age, color, legs properties and create different methods
+
+=== Solution ===
+*/
+class Animal {
+  constructor(name, age, color, legs) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+  }
+  getAnimalInfo() {
+    return `${this.name} is ${this.age} years old, ${this.color}, and has ${this.legs} legs.`;
+  }
+}
+
+/*
+Create a Dog and Cat child class from the Animal Class.
+
+=== Solution ===
+*/
