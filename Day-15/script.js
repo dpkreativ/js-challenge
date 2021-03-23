649fdcb603963d726f0e4e5b785da2d30987ab8c
Divine <dpkreativ@gmail.com>
2021-03-23 07:15:25 +0100

    free code camp algo challenge

--- Day-15/script.js
+++ Day-15/script.js
@@ -13,4 +13,23 @@ const countdown = (n) => {
   }
 };
 
-console.log(countdown(5));
+// console.log(countdown(5));
+
+/*
+We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+*/
+// === Solution ===
+
+const rangeOfNumbers = (startNum, endNum) => {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const countArray = rangeOfNumbers(startNum, endNum - 1);
+    countArray.push(endNum);
+    return countArray;
+  }
+};
+
+console.log(rangeOfNumbers(1, 5));
+console.log(rangeOfNumbers(6, 9));
+console.log(rangeOfNumbers(4, 4));
