8247182042ff923eeba6836f69784ff00bab10d9
Divine Philip <dpkreativ@gmail.com>
2021-04-07 11:45:44 +0100

    done with exercise 1

--- Day-17/main.js
+++ Day-17/main.js
@@ -4,4 +4,9 @@ Store you first name, last name, age, country, city in your browser localStorage
 
 === Solution ===
 */
+localStorage.setItem("firstName", "Divine");
+localStorage.setItem("lastName", "Philip");
+localStorage.setItem("age", 24);
+localStorage.setItem("country", "Nigeria");
+localStorage.setItem("city", "Abia");
 console.log(localStorage);
