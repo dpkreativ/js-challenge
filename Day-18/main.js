a46c4b5fe2050a28de7fee0cafc0dd324f70f98f
Divine Philip <dpkreativ@gmail.com>
2021-04-10 02:51:25 +0100

    finished exercise 2 num 1

--- Day-18/main.js
+++ Day-18/main.js
@@ -6,17 +6,30 @@ Read the countries API using fetch and print the name of country, capital, langu
 
 === Solution ===
 */
-fetch(countriesAPI)
+// fetch(countriesAPI)
+//   .then((response) => response.json())
+//   .then((data) =>
+//     data.forEach((obj) =>
+//       console.log({
+//         name: obj.name,
+//         capital: obj.capital,
+//         languages: obj.languages,
+//         population: obj.population,
+//         area: obj.area,
+//       })
+//     )
+//   )
+//   .catch((err) => console.log(err));
+
+// Exercise 2
+/*1
+Print out all the cat names in to catNames variable.
+
+=== Solution ===
+*/
+const catNames = fetch(catsAPI)
   .then((response) => response.json())
-  .then((data) =>
-    data.forEach((obj) =>
-      console.log({
-        name: obj.name,
-        capital: obj.capital,
-        languages: obj.languages,
-        population: obj.population,
-        area: obj.area,
-      })
-    )
-  )
+  .then((data) => data.map((obj) => obj.name))
   .catch((err) => console.log(err));
+
+console.log(catNames);
