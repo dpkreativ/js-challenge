f656d1ba0213b3523671f38a0a36d9e761dcae60
Divine Philip <dpkreativ@gmail.com>
2021-04-10 02:10:17 +0100

    added api url links

--- Day-18/main.js
+++ Day-18/main.js
@@ -1,3 +1,5 @@
+const countriesAPI = "https://restcountries.eu/rest/v2/all";
+const catsAPI = "https://api.thecatapi.com/v1/breeds";
 // Exercise 1
 /*1
 Read the countries API using fetch and print the name of country, capital, languages, population and area.
