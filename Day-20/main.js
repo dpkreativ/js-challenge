2d3d4cdd0822eeb0172fb50b575dfef821e4f1ca
Divine Philip <dpkreativ@gmail.com>
2021-04-20 16:37:38 +0100

    done with day 20

---
+++ Day-20/main.js
@@ -0,0 +1,4 @@
+// Today we just learned how to write clean code... I think I'm going to study AirBnb's guidlines and Google's guides to understand how to write clean code better.
+// Google: https://google.github.io/styleguide/jsguide.html
+// Airbnb: https://github.com/airbnb/javascript
+// Standard JS: https://standardjs.com/
