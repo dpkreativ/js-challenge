38d1699a3517c583384ceeb0311b72abac6469a3
Divine Philip <dpkreativ@gmail.com>
2021-04-28 16:44:41 +0100

    solved hackerrank algo challenge

---
+++ Day-21/script.js
@@ -0,0 +1,22 @@
+/*
+Given a string, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+*/
+
+const anagram = (s) => {
+  if (s.length % 2 !== 0) {
+    return -1;
+  }
+  sToArray = s.split("");
+  firstHalf = sToArray.slice(0, sToArray.length / 2);
+  secondHalf = sToArray.slice(sToArray.length / 2, sToArray.length);
+  count = [];
+  firstHalf.forEach((arr) => (secondHalf.includes(arr) ? "" : count.push(arr)));
+  return count.length;
+};
+
+console.log(anagram("aaabbb"));
+console.log(anagram("ab"));
+console.log(anagram("abc"));
+console.log(anagram("mnop"));
+console.log(anagram("xyyx"));
+console.log(anagram("xaxbbbxx"));
