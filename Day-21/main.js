ff994e7d6f19e31a7caa6c6f3e8bec0b1b90576d
Divine Philip <dpkreativ@gmail.com>
2021-05-25 03:44:40 +0100

    main.js setup
    - create the main.js file
    - set up the questions and solution spaces for exercise 1

---
+++ Day-21/main.js
@@ -0,0 +1,36 @@
+// Exercise 1
+/*1
+Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+
+=== Solution ===
+*/
+
+/*2
+Get get each of the the paragraph using document.querySelector('#id') and by their id
+
+=== Solution ===
+*/
+
+/*3
+Get all the p as nodeList using document.querySelectorAll(tagname) and by their tag name
+
+=== Solution ===
+*/
+
+/*4
+Loop through the nodeList and get the text content of each paragraph
+
+=== Solution ===
+*/
+
+/*5
+Set a text content to paragraph the fourth paragraph,Fourth Paragraph
+
+=== Solution ===
+*/
+
+/*6
+Set id and class attribute for all the paragraphs using different attribute setting methods
+
+=== Solution ===
+*/
